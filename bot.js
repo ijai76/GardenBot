daa3cc5a38975cfd13729160e0713adc5149875e
vcto_ <82398490+notvcto@users.noreply.github.com>
2025-05-29 16:21:12 -0400

    Increased buffer to 10000ms

--- bot.js
+++ bot.js
@@ -30,7 +30,7 @@ const scheduleStockCheck = () => {
   const delay = next - now;
   setTimeout(
     async () => {
-      const bufferMs = 9000;
+      const bufferMs = 10000;
       await new Promise((r) => setTimeout(r, bufferMs));
 
       try {
